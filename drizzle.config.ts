53701eabcb318b093a01f429119513e617214132
abbasjay <abbasjabbar001@gmail.com>
2024-11-07 21:36:08 +0000

    next cors

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,9 @@ import "./envConfig"; // If envConfig.ts is in the same directory as drizzle.con
 // Or use the correct relative path if it's in a different location
 
 import { defineConfig } from "drizzle-kit";
-import { resolve } from "path";
 
 export default defineConfig({
-  schema: 'lib/db/schema.ts',
+  schema: "lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.POSTGRES_URL || "",
