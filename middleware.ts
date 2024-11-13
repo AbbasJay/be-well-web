07c7b8ac5a61719b195a67e54c6b20a5ad11e36c
Marat Davudov <marat.davudov@sky.uk>
2024-11-13 19:31:56 +0000

    fixing CORS headers

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const allowedOrigins = [
+  "http://localhost:8081",           
+  "https://be-well-web.vercel.app", 
+  "http://localhost:3000"
+];
+
 export function middleware(request: NextRequest) {
   // Only apply to API routes
   if (request.nextUrl.pathname.startsWith("/api")) {
@@ -16,9 +22,13 @@ export function middleware(request: NextRequest) {
       });
     }
 
+    const origin = request.headers.get("origin");
+    const isAllowedOrigin = allowedOrigins.includes(origin || "");
+
     // Add CORS headers to the response
     const response = NextResponse.next();
-    response.headers.set("Access-Control-Allow-Origin", "*");
+    //response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Origin", isAllowedOrigin ? origin || "" : "");
     response.headers.set(
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, DELETE, OPTIONS"
