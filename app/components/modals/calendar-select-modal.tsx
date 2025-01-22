710a5aa2269265409a231976dd912231cfb2f34b
Hussomuss <hussain_jabbar1@hotmail.com>
2025-01-22 22:19:26 +0000

    mapped through current events selected and formatted date with title of event

--- app/components/modals/calendar-select-modal.tsx
+++ app/components/modals/calendar-select-modal.tsx
@@ -1,20 +1,37 @@
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
-import Calendar from "../calendar/Calendar";
-import { EventApi } from "@fullcalendar/core";
+import { EventApi, formatDate } from "@fullcalendar/core";
+
+interface CalendarSelectModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  currentEvents: EventApi[];
+}
 
 export default function CalendarSelectModal({
   open,
   onOpenChange,
   currentEvents,
-}: {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
-  currentEvents: EventApi[];
-}) {
+}: CalendarSelectModalProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogTitle>Select a date</DialogTitle>
-      <DialogContent></DialogContent>
+      <DialogTitle>All Events</DialogTitle>
+      <DialogContent>
+        <ul className="space-y-2">
+          {currentEvents.map((event) => (
+            <li key={event.id} className="flex flex-col">
+              <span className="font-bold">
+                {event.start &&
+                  formatDate(event.start, {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+              </span>
+              <span className="italic">{event.title}</span>
+            </li>
+          ))}
+        </ul>
+      </DialogContent>
     </Dialog>
   );
 }
