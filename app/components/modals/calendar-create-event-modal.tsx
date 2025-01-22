d818ea7d9141343f246f0b3413048b12f2e83c04
Hussomuss <hussain_jabbar1@hotmail.com>
2025-01-22 22:25:32 +0000

    Interface creation for calender create event

--- app/components/modals/calendar-create-event-modal.tsx
+++ app/components/modals/calendar-create-event-modal.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 
-export default function CalendarCreateEventModal() {
+interface CalendarCreateEventModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export default function CalendarCreateEventModal({
+  open,
+  onOpenChange,
+}: CalendarCreateEventModalProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogTitle>Create Event</DialogTitle>
