a9263010a06aad4260a76e0910311023a1a52549
abbasjay <abbasjabbar001@gmail.com>
2025-01-23 01:35:00 +0000

    comment out swagger files

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,12 +1,16 @@
-import { getApiDocs } from "@/lib/swagger";
-import ReactSwagger from "./react-swagger";
-import { OpenAPIV3 } from "openapi-types";
+// // import { getApiDocs } from "@/lib/swagger";
+// // import ReactSwagger from "./react-swagger";
+// // import { OpenAPIV3 } from "openapi-types";
 
-export default async function IndexPage() {
-  const spec = await getApiDocs();
-  return (
-    <section className="container">
-      {/* <ReactSwagger spec={spec as OpenAPIV3.Document} /> */}
-    </section>
-  );
+// export default async function IndexPage() {
+//   const spec = await getApiDocs();
+//   return (
+//     <section className="container">
+//       {/* <ReactSwagger spec={spec as OpenAPIV3.Document} /> */}
+//     </section>
+//   );
+// }
+
+export default function IndexPage() {
+  return <div>Hello World</div>;
 }
--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -1,35 +1,39 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-import "swagger-ui-react/swagger-ui.css";
-import { OpenAPIV3 } from "openapi-types";
+// import { useEffect, useState } from "react";
+// import dynamic from "next/dynamic";
+// import "swagger-ui-react/swagger-ui.css";
+// import { OpenAPIV3 } from "openapi-types";
 
-const SwaggerUI = dynamic(() => import("swagger-ui-react"), {
-  ssr: false,
-  loading: () => <div>Loading API documentation...</div>,
-});
+// const SwaggerUI = dynamic(() => import("swagger-ui-react"), {
+//   ssr: false,
+//   loading: () => <div>Loading API documentation...</div>,
+// });
 
-type Props = {
-  spec: OpenAPIV3.Document;
-};
+// type Props = {
+//   spec: OpenAPIV3.Document;
+// };
 
-function ReactSwagger({ spec }: Props) {
-  const [mounted, setMounted] = useState(false);
+// function ReactSwagger({ spec }: Props) {
+//   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+//   useEffect(() => {
+//     setMounted(true);
+//   }, []);
 
-  if (!mounted) {
-    return <div>Loading API documentation...</div>;
-  }
+//   if (!mounted) {
+//     return <div>Loading API documentation...</div>;
+//   }
 
-  return (
-    <div className="swagger-wrapper">
-      <SwaggerUI spec={spec} />
-    </div>
-  );
+//   return (
+//     <div className="swagger-wrapper">
+//       <SwaggerUI spec={spec} />
+//     </div>
+//   );
+// }
+
+// export default ReactSwagger;
+
+export default function ReactSwagger() {
+  return <div>Hello World</div>;
 }
-
-export default ReactSwagger;
