2a6d50dec19e4993726cd261703b27e614649437
abbasjay <abbasjabbar001@gmail.com>
2025-01-23 01:24:11 +0000

    removed standalone

--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -4,14 +4,21 @@ import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
 import { OpenAPIV3 } from "openapi-types";
 
-const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), {
+  ssr: false,
+  loading: () => <div>Loading API documentation...</div>,
+});
 
 type Props = {
   spec: OpenAPIV3.Document;
 };
 
 function ReactSwagger({ spec }: Props) {
-  return <SwaggerUI spec={spec} />;
+  return (
+    <div className="swagger-wrapper">
+      <SwaggerUI spec={spec} />
+    </div>
+  );
 }
 
 export default ReactSwagger;
