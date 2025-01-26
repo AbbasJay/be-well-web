8f93f659d7fc267ec4ce70a2343019a641cfe737
Hussomuss <hussain_jabbar1@hotmail.com>
2025-01-26 14:52:53 +0000

    imported EventApi from library

--- app/components/modals/calendar-event-actions-modal.tsx
+++ app/components/modals/calendar-event-actions-modal.tsx
@@ -6,13 +6,24 @@ import {
   DialogFooter,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { EventApi } from "@fullcalendar/core";
 
 interface CalendarEventActionsModalProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  event: EventApi;
 }
 
 export default function CalendarEventActionsModal({
   open,
   onOpenChange,
-}: CalendarEventActionsModalProps) {}
+  event,
+}: CalendarEventActionsModalProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogTitle>Current Event: {event?.title}</DialogTitle>
+      </DialogContent>
+    </Dialog>
+  );
+}
