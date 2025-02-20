15a711bc511fad47f74271b822dced7e0a0affa3
Marat Davudov <marat.davudov@sky.uk>
2025-02-20 22:34:28 +0000

    [FIX] return top-5 closes businesses if none satisfied the search

--- app/api/mobile/businesses/filter/route.ts
+++ app/api/mobile/businesses/filter/route.ts
@@ -139,7 +139,7 @@ export async function POST(request: NextRequest) {
     // and filter out businesses that are beyond the maximum distance
     // from the user
     // we use haversine formula to calculate the distance
-    const filtered_businesses = businesses.filter(
+    let filtered_businesses = businesses.filter(
       (business) =>
         calculateDistance(
           parseFloat(business.latitude!),
@@ -150,6 +150,27 @@ export async function POST(request: NextRequest) {
     );
 
     console.log(filtered_businesses);
+    
+    //if no businesses are found return top 5 closest businesses
+    if (filtered_businesses.length === 0) {
+      filtered_businesses = businesses
+        .sort(
+          (a, b) =>
+            calculateDistance(
+              parseFloat(a.latitude!),
+              parseFloat(a.longitude!),
+              lat,
+              lng
+            ) -
+            calculateDistance(
+              parseFloat(b.latitude!),
+              parseFloat(b.longitude!),
+              lat,
+              lng
+            )
+        )
+        .slice(0, 5);
+    }
 
     // return the filtered businesses
     return NextResponse.json(filtered_businesses, { status: 200 });
